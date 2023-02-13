298b70e8d3280664ffb0cbf1a1873ad396980588
bingliang.hu <bingliang.hu@advance.ai>
2023-02-13 12:50:07 +0800

    update playwrighte

--- apps/nest/src/app/app.controller.ts
+++ apps/nest/src/app/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import playwright from 'playwright';
 
 import { AppService } from './app.service';
 
@@ -9,5 +10,21 @@ export class AppController {
   @Get()
   getData() {
     return this.appService.getData();
+  }
+}
+@Controller('pdf')
+export class PDFController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  async getData() {
+    const browser = await playwright.chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.emulateMedia({ media: 'screen' });
+    await page.pdf({ path: `document.pdf` });
+    await browser.close();
+    return { message: 'success' };
   }
 }
